9a090d8f432b22753ea3a7ee41bfdaae1537e841
deni <108141159+danielddenial@users.noreply.github.com>
2024-11-12 12:23:57 -0500

    Add files via upload

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,14 @@
-# Only one single output remains unspent from block 123,321. What address was it sent to?
+#!/bin/bash
+
+hash_123321=$(./bitcoin-cli getblockhash 123321)
+
+./bitcoin-cli getblock $hash_123321 | jq -r '.tx[]' | while read TXID; do
+    if ./bitcoin-cli getrawtransaction $TXID true | jq -e ".vin[]" > /dev/null; then
+        echo "$TXID"
+        break
+    fi
+done
+
+
+
+
