6ca6173fed080bb37fb405f918a69bac60919d5c
deni <108141159+danielddenial@users.noreply.github.com>
2024-11-12 13:46:08 -0500

    Fixed 007

--- submission/007.sh
+++ submission/007.sh
@@ -2,11 +2,17 @@
 
 hash_123321=$(bitcoin-cli getblockhash 123321)
 
-bitcoin-cli getblock $hash_123321 | jq -r '.tx[]' | while read TXID; do
-    if bitcoin-cli getrawtransaction $TXID true | jq -e ".vin[]" > /dev/null; then
-        echo "$TXID"
-        break
-    fi
+for txid in $(bitcoin-cli getblock $hash_123321 | jq -r '.tx[]'); do
+    bitcoin-cli getrawtransaction $txid true | jq -c '.vout[]' | while read output; do
+        vout_index=$("$output" | jq -r '.n')
+        spent=$(bitcoin-cli gettxout $txid $vout_index)
+
+        if [ -n "$spent" ]; then
+            address=$("$output" | jq -r '.scriptPubKey.addresses[0]')
+            echo "$address"
+            break 2
+        fi
+    done
 done
 
 
