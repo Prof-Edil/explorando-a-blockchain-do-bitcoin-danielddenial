67abeae2701f323c12669349e732c657fa14335d
deni <108141159+danielddenial@users.noreply.github.com>
2024-11-12 12:57:03 -0500

    Solution 007

--- submission/007.sh
+++ submission/007.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-hash_123321=$(./bitcoin-cli getblockhash 123321)
+hash_123321=$(bitcoin-cli getblockhash 123321)
 
-./bitcoin-cli getblock $hash_123321 | jq -r '.tx[]' | while read TXID; do
-    if ./bitcoin-cli getrawtransaction $TXID true | jq -e ".vin[]" > /dev/null; then
+bitcoin-cli getblock $hash_123321 | jq -r '.tx[]' | while read TXID; do
+    if bitcoin-cli getrawtransaction $TXID true | jq -e ".vin[]" > /dev/null; then
         echo "$TXID"
         break
     fi
