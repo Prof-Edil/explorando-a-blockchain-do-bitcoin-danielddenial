f1c851210f6e5a915d06d08545f959dfa82f372d
deni <108141159+danielddenial@users.noreply.github.com>
2025-02-02 17:29:05 -0300

    Tirando ./ do 008

--- submission/008.sh
+++ submission/008.sh
@@ -2,7 +2,7 @@
 
 tx="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
 
-raw_tx=$(./bitcoin-cli getrawtransaction "$tx" true)
+raw_tx=$(bitcoin-cli getrawtransaction "$tx" true)
 
 witness=$(echo "$raw_tx" | jq -r '.vin[0].txinwitness[2]')
 
