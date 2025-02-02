de9a1840769c1e95dfaa6699888b20de04386d29
deni <108141159+danielddenial@users.noreply.github.com>
2025-02-02 15:05:29 -0300

    Add files via upload

--- submission/007.sh
+++ submission/007.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-Hash=$(./bitcoin-cli getblockhash 123321)
-Block=$(./bitcoin-cli getblock $Hash | jq -r '.tx[]')
+Hash=$(bitcoin-cli getblockhash 123321)
+Block=$(bitcoin-cli getblock $Hash | jq -r '.tx[]')
 
 for txid in $Block; do
 
-  tx=$(./bitcoin-cli getrawtransaction $txid true)
+  tx=$(bitcoin-cli getrawtransaction $txid true)
   vout_count=$(echo "$tx" | jq '.vout | length')
 
   for (( i=0; i<$vout_count; i++ )); do
-    address=$(./bitcoin-cli gettxout $txid $i)
+    address=$(bitcoin-cli gettxout $txid $i)
 
     if [[ ! -z "$address" ]]; then
       echo $address | jq -r '.scriptPubKey.address'
